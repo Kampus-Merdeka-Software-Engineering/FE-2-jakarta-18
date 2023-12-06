43aaf74761092627834f95585942a579dbd3856e
DESKTOP-GI0R5E1\prili <prilianatianarti@gmail.com>
2023-12-06 20:32:57 +0700

    merapihkan tampilan

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log(200)
+fetch('http://localhost:3000/products', {
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    method: 'GET'
+})
+.then((result) => result.json())
+.then((result) => {
+    console.log(result)
+})
+.catch((err) => {
+    console.log(err)
+})
+
+// fetch('http://localhost:3000/products', {
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     method: 'POST',
+//     body: JSON.stringify({
+//         name: "Donut",
+//         price: 5000,
+//         description: "text"
+//     })
+// })
+// .then((result) => result.json())
+// .then((result) => {
+//     console.log(result)
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
