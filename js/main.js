f7345f1666f4e5c199f15ee12603034e58fdb598
DESKTOP-GI0R5E1\prili <prilianatianarti@gmail.com>
2023-12-07 18:29:36 +0700

    edit content

--- js/main.js
+++ js/main.js
@@ -35,25 +35,23 @@
 
 document.getElementById('myForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the default form submission
-    
+
     // Get form values
     const name = document.getElementById('nameInput').value;
-    const phoneNumber = document.getElementById('phoneInput').value;
+    const phone_number = document.getElementById('phoneInput').value;
     const email = document.getElementById('emailInput').value;
     const message = document.getElementById('messageInput').value;
 
     // Create data object
     const data = {
         name: name,
-        phoneNumber: phoneNumber,
+        phone_number: phone_number,
         email: email,
         message: message
     };
 
-    console.log(data)
-
     // Make the fetch request
-    fetch('http://localhost:3000/products', {
+    fetch('http://localhost:3000/contacts', {
         headers: {
             'Content-Type': 'application/json'
         },
@@ -64,9 +62,20 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
     .then(result => {
         console.log(result);
         // Handle the response from the server as needed
+
+        // Clear form fields
+        document.getElementById('nameInput').value = '';
+        document.getElementById('phoneInput').value = '';
+        document.getElementById('emailInput').value = '';
+        document.getElementById('messageInput').value = '';
+
+        // Display success alert
+        alert('Pesan telah terkirim!');
     })
     .catch(err => {
         console.error(err);
         // Handle errors
+        // Optionally display an error alert
+        alert('Gagal mengirim pesan. Silakan coba lagi.');
     });
 });
